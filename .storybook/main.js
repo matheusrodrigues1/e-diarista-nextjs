fccf9e9d9608ac5884e84784d7159616c9d2c9d9
matheusrodrigues1 <matech00@outlook.com>
2024-08-05 20:34:58 -0300

    criando primeiro component com storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -17,10 +17,7 @@ module.exports = {
 
     return config;
   },
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
---
+++ src/ui/components/inputs/RoundedButton/RoundedButton.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import RoundedButton from "./RoundedButton";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+export default {
+  title: "inputs/RoundedButton",
+  component: RoundedButton,
+} as ComponentMeta<typeof RoundedButton>;
+
+const Template: ComponentStory<typeof RoundedButton> = (args) => (
+  <RoundedButton {...args}>Click</RoundedButton>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  variant: "contained",
+};
