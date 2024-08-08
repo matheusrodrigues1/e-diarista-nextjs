7651702e7937fc955752234ccc711a4397110c18
matheusrodrigues1 <matech00@outlook.com>
2024-08-08 14:23:06 -0300

    construindo component de titulo

--- src/ui/partials/index/_advantages.tsx
+++ src/ui/partials/index/_advantages.tsx
@@ -1,7 +1,14 @@
-import { GradientBackground } from "./_advantages.style";
+import { Container } from "@mui/material";
+import { GradientBackground, SectionTitle } from "./_advantages.style";
 
 const Advantages = () => {
-  return <GradientBackground>asfdfsdfsd</GradientBackground>;
+  return (
+    <GradientBackground>
+      <Container>
+        <SectionTitle>Por que usar o E-diarista?</SectionTitle>
+      </Container>
+    </GradientBackground>
+  );
 };
 
 export default Advantages;
