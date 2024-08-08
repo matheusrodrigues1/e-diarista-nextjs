ff5d8fe1e5a16d7e8231d446bf30f674c8dda083
matheusrodrigues1 <matech00@outlook.com>
2024-08-08 15:24:05 -0300

    listando todas as vantagens

--- src/ui/partials/index/_advantages.tsx
+++ src/ui/partials/index/_advantages.tsx
@@ -2,58 +2,75 @@ import { Avatar, Container, ListItem, ListItemAvatar } from "@mui/material";
 import {
   AvatarStyled,
   GradientBackground,
+  ListDivider,
   ListItemTextStyled,
   ListStyle,
   SectionTitle,
 } from "./_advantages.style";
+import React from "react";
 
 const Advantages = () => {
+  const advantagesList = [
+    {
+      icon: "twf-woman",
+      title: "Diversidade",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, non.",
+    },
+    {
+      icon: "twf-certificate",
+      title: "Confiabilidade",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, non.",
+    },
+    {
+      icon: "twf-search-2",
+      title: "Rastreabilidade",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, non.",
+    },
+    {
+      icon: "twf-frame-broken",
+      title: "Segurança",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, non.",
+    },
+    {
+      icon: "twf-payment",
+      title: "Controle",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, non.",
+    },
+    {
+      icon: "twf-broom-bucket",
+      title: "Experiência",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, non.",
+    },
+  ];
   return (
     <GradientBackground>
       <Container>
         <SectionTitle>Por que usar o E-diarista?</SectionTitle>
         <ListStyle>
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled></AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Deversidade"} secondary={"5mil"} />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled></AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Deversidade"} secondary={"5mil"} />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled></AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Deversidade"} secondary={"5mil"} />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled></AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Deversidade"} secondary={"5mil"} />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled></AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Deversidade"} secondary={"5mil"} />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled></AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Deversidade"} secondary={"5mil"} />
-          </ListItem>
+          {advantagesList.map((item, index) => {
+            return (
+              <React.Fragment key={item.icon}>
+                {index !== 0 && <ListDivider />}
+                <ListItem>
+                  <ListItemAvatar>
+                    <AvatarStyled>
+                      <i className={item.icon}></i>
+                    </AvatarStyled>
+                  </ListItemAvatar>
+                  <ListItemTextStyled
+                    primary={item.title}
+                    secondary={item.description}
+                  />
+                </ListItem>
+              </React.Fragment>
+            );
+          })}
         </ListStyle>
       </Container>
     </GradientBackground>
