b3c40ed4840d25f8df6765aa35305605bab51d72
matheusrodrigues1 <matech00@outlook.com>
2024-08-08 12:04:06 -0300

    estrutura base da tela de apresentacao

--- src/ui/partials/index/_presentation.tsx
+++ src/ui/partials/index/_presentation.tsx
@@ -1,14 +1,9 @@
-import { SectionContainer } from "./_presentation.styled";
+import { ContainerStyled, SectionContainer } from "./_presentation.styled";
 
 const Presentation = () => {
   return (
     <SectionContainer>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam nostrum
-      distinctio qui eos praesentium ex deserunt, in nulla saepe, ullam
-      perferendis adipisci officia suscipit libero tenetur necessitatibus quidem
-      laborum quod architecto exercitationem? Debitis obcaecati laboriosam quas
-      odio atque repellat praesentium nesciunt natus quia optio hic
-      necessitatibus quam, fugiat aliquam nostrum.
+      <ContainerStyled></ContainerStyled>
     </SectionContainer>
   );
 };
